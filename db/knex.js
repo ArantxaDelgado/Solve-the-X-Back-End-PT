4726edd3694b7526f50869b860133abdaa572b91
ardelgad <95305208+ardelgad@users.noreply.github.com>
2022-11-19 19:07:18 +0100

    API Rest completed

--- db/knex.js
+++ db/knex.js
@@ -2,7 +2,9 @@ const knex = require("knex");
 
 const connectedKnex = knex({
     client : "sqlite3",
-    connecton: {
-        filename: "dataBase.sqlite3" //nombre de mi base de datos
+    connection: {
+        filename: "dataBase.sqlite3"
     }
 });
+
+module.exports = connectedKnex;
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const db = require("./db/todos");
 
-app.get("/test", (req, res) => {
-    res.status(200).json({succes: true});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/todos", async(req, res) => {
+    const results = await db.createTodo(req.body);
+    res.status(201).json({id: results [0]});
 });
 
-app.listen(1337, () => console.log("server is runnig on port"));
+app.get("/todos", async(req, res) => {
+    const todos = await db.getAllTodos();
+    res.status(200).json({todos});
+});
+
+
+app.patch("/todos/:id", async(req, res) => {
+    const id = await db.updateTodo(req.params.id, req.body);
+    res.status(200).json({id});
+});
+
+
+app.delete("/todos/:id", async(req, res) => {
+    await db.deleteTodo(req.params.id);
+    res.status(200).json({success: true});
+});
+
+
+app.listen(3000, () => console.log("server is runnig on port 3000"));
